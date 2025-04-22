95fce648e3dfe54511652424f43094e063a3458d
pratikdahikar22 <137139428+pratikdahikar22@users.noreply.github.com>
2025-04-22 17:41:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ def home():
     return "Hello, World! Welcome to your Flask app 1."
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=False)
+    app.run(host='0.0.0.0', port=8000, debug=False)
 
